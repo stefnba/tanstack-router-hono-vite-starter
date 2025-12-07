eae7324690f39376a854facbd80b518aa42b4330
Stefan Bauer <stefanjakob.bauer@gmail.com>
2025-12-07 10:02:19 +0100

    feat(client): add Dialog and Drawer components

--- packages/client/src/components/ui/field.tsx
+++ packages/client/src/components/ui/field.tsx
@@ -46,7 +46,8 @@ function FieldGroup({ className, ...props }: React.ComponentProps<'div'>) {
         <div
             data-slot="field-group"
             className={cn(
-                'group/field-group @container/field-group flex w-full flex-col gap-7 data-[slot=checkbox-group]:gap-3 [&>[data-slot=field-group]]:gap-4',
+                'group/field-group @container/field-group flex w-full flex-col gap-7 data-[slot=checkbox-group]:gap-3',
+                '*:data-[slot=field-group]:gap-4',
                 className
             )}
             {...props}
@@ -57,15 +58,15 @@ function FieldGroup({ className, ...props }: React.ComponentProps<'div'>) {
 const fieldVariants = cva('group/field flex w-full gap-3 data-[invalid=true]:text-destructive', {
     variants: {
         orientation: {
-            vertical: ['flex-col [&>*]:w-full [&>.sr-only]:w-auto'],
+            vertical: ['flex-col *:w-full [&>.sr-only]:w-auto'],
             horizontal: [
                 'flex-row items-center',
-                '[&>[data-slot=field-label]]:flex-auto',
+                '*:data-[slot=field-label]:flex-auto',
                 'has-[>[data-slot=field-content]]:items-start has-[>[data-slot=field-content]]:[&>[role=checkbox],[role=radio]]:mt-px',
             ],
             responsive: [
-                'flex-col [&>*]:w-full [&>.sr-only]:w-auto @md/field-group:flex-row @md/field-group:items-center @md/field-group:[&>*]:w-auto',
-                '@md/field-group:[&>[data-slot=field-label]]:flex-auto',
+                'flex-col *:w-full [&>.sr-only]:w-auto @md/field-group:flex-row @md/field-group:items-center @md/field-group:*:w-auto',
+                '@md/field-group:*:data-[slot=field-label]:flex-auto',
                 '@md/field-group:has-[>[data-slot=field-content]]:items-start @md/field-group:has-[>[data-slot=field-content]]:[&>[role=checkbox],[role=radio]]:mt-px',
             ],
         },
